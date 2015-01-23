38c9471e13a6a3ca18b1ae1b8bf7b169e996cdfa
Daniel Mesa <danielromanmesa@gmail.com>
2015-01-22 20:32:40 -0500

    Interview questions for job interview applicants. Will keep adding on as necessary.

---
+++ interview/src/main/java/com/nothingtothetable/interview/executable/Interview2.java
@@ -0,0 +1,10 @@
+package com.nothingtothetable.interview.executable;
+
+public class Interview2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ interview/src/main/java/com/nothingtothetable/interview/questions/RemoveDuplicates.java
@@ -0,0 +1,14 @@
+package com.nothingtothetable.interview.questions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RemoveDuplicates {
+	
+	public static String removeDuplicates(final String input)
+	{
+		
+		
+		return "";
+	}
+}
