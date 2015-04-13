83e721202b2e0e22f5234e61dc49688714f1ea97
Daniel Mesa <danielromanmesa@gmail.com>
2015-04-13 16:12:10 -0400

    Realized that I accidentally committed a candidate's code instead of mine. Swapping it out and also adding in null checks.

--- src/main/java/com/nothingtothetable/interview/questions/RemoveDuplicates.java
+++ src/main/java/com/nothingtothetable/interview/questions/RemoveDuplicates.java
@@ -10,17 +10,6 @@ public class RemoveDuplicates {
 	
 	public static String removeDuplicates(final String input)
 	{
-		HashSet<Character> set = new HashSet<Character>();
-		StringBuilder str = new StringBuilder();
-		if(input==null||input.length()==0) return "";
-		for(int i=0; i<input.length(); i++)
-		{
-			if(!set.contains(input.charAt(i)))
-			{
-				set.add(input.charAt(i));
-				str.append(input.charAt(i));
-			}
-		}
-		return str.toString(); 
+		return "";
 	}
 }
--- src/main/java/com/nothingtothetable/interview/questions/RemoveDuplicatesAnswer.java
+++ src/main/java/com/nothingtothetable/interview/questions/RemoveDuplicatesAnswer.java
@@ -1,13 +1,36 @@
 package com.nothingtothetable.interview.questions;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
-public class RemoveDuplicatesAnswer {
-	
+public class RemoveDuplicatesAnswer
+{
+
 	public static String removeDuplicates(final String input)
 	{
-	
-		return "";
+		if(input == null || input.length() == 0)
+		{
+			return "";
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		Map<Character, Integer> duplicates = new HashMap<Character, Integer>();
+
+		for (char c : input.toCharArray())
+		{
+			if (duplicates.get(c) == null)
+			{
+				sb.append(c);
+				duplicates.put(c, 1);
+			} else
+			{
+
+				duplicates.put(c, duplicates.get(c) + 1);
+			}
+		}
+
+		return sb.toString();
 	}
+
 }
